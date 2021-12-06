51ddb008f3b4de4df1a962e210534b215854b2aa
hsinyi lin <10656025@ntub.edu.tw>
2021-12-06 11:40:41 +0800

    feat(coffees): add coffee models

--- coffees/models.py
+++ coffees/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 
 # Create your models here.
+from utils.models import TimestampModel
 
-class Roast(models.Choices):
+
+class Roast(models.TextChoices):
     # enum = value(存入的值), display
     LIGHT = 'Light', '極淺度烘焙'
     CINNAMON = 'Cinnamon', '淺度烘焙'
@@ -18,8 +20,37 @@ class Roast(models.Choices):
 class OriginPlace(models.Model):
     name = models.CharField('名稱', max_length=20, unique=True)
 
+    def __str__(self):
+        return self.name
 
-class Coffee(models.Model):
+    class Meta:
+        verbose_name = '產地'  # 單數處理
+        verbose_name_plural = '產地'  # 複數處理
+
+
+class MainProcessing(models.Model):
+    name = models.CharField('名稱', max_length=20, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = '主要處理法'
+        verbose_name_plural = '主要處理法'
+
+
+class Grinding(TimestampModel):
+    name = models.CharField('名稱', max_length=10, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = '磨豆方式'
+        verbose_name_plural = '磨豆方式'
+
+
+class Coffee(TimestampModel):
     name = models.CharField('名稱', max_length=20, unique=True)
     weight = models.PositiveIntegerField('重量')
     taste = models.TextField('味道')
@@ -30,6 +61,18 @@ class Coffee(models.Model):
     origin_place = models.ForeignKey(
         OriginPlace,
         on_delete=models.PROTECT,
+        verbose_name='產地'
+    )
+    main_processing = models.ForeignKey(
+        MainProcessing,
+        on_delete=models.PROTECT,
         verbose_name='主要處理法'
     )
+    grindings = models.ManyToManyField(Grinding, verbose_name='磨豆方式')
 
+    def __str__(self):
+        return f'{self.name}({self.price})'
+
+    class Meta:
+        verbose_name = '咖啡'
+        verbose_name_plural = '咖啡'
