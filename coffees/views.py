5e10d204e2b8c43ea573d084d69a16949fe3531f
hsinyi <10656025@ntub.edu.tw>
2021-12-20 10:38:03 +0800

    feat(coffees): add index page

--- coffees/views.py
+++ coffees/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from coffees.models import Coffee
 
 
 # Create your views here.
+
+
 def index(request):
-    return HttpResponse('<h1>咖啡</h1>')
+    coffees = Coffee.objects.all()
+    return render(request, 'coffees/index.html', {'coffees': coffees})
