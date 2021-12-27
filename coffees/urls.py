b7f43b27049ce601cb201b929283214ab63339b2
hsinyi <10656025@ntub.edu.tw>
2021-12-27 09:57:15 +0800

    feat(coffees): add edit page

--- coffees/urls.py
+++ coffees/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:pk>/', views.show, name='show'),
     path('add/', views.add, name='add'),
+    path('<int:pk>/edit/', views.edit, name='edit'),
 ]
