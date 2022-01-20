cbd0f6eaf21c28e0211830027b93506ec177b37a
hsinyi <10656025@ntub.edu.tw>
2022-01-20 13:53:28 +0800

    feat(auth): add login page

--- core/settings.py
+++ core/settings.py
@@ -123,6 +123,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+LOGIN_URL = '/login/'
+
+LOGIN_REDIRECT_URL = '/'
+
+LOGOUT_REDIRECT_URL = '/login/'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- core/urls.py
+++ core/urls.py
@@ -14,9 +14,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
 
+from django.contrib.auth.views import LoginView, LogoutView
+
+login_params = {
+    'template_name': 'users/login.html',
+    'redirect_authenticated_user': True,
+}
+
+
+
 urlpatterns = [
+    path('', lambda request: redirect('coffees:index'), name='root'),
+    path('login/', LoginView.as_view(**login_params), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('coffees/', include('coffees.urls')),
     path('admin/', admin.site.urls),
 ]
