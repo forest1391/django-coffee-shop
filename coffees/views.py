12d222e90a9926da45346381339765ccd6967351
hsinyi <10656025@ntub.edu.tw>
2022-01-20 14:59:28 +0800

    feat(coffees): add permissions

--- coffees/views.py
+++ coffees/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required, permission_required
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 
@@ -10,16 +11,20 @@ from coffees.models import Coffee
 from utils.forms import DeleteConfirmForm
 
 
+@login_required
 def index(request):
     coffees = Coffee.objects.all()
     return render(request, 'coffees/index.html', {'coffees': coffees})
 
 
+@login_required
 def show(request, pk):
     coffee = get_object_or_404(Coffee, pk=pk)
     return render(request, 'coffees/show.html', {'coffee': coffee})
 
 
+@login_required
+@permission_required('coffees.add_coffee', raise_exception=True)
 def add(request):
     form = CoffeeForm(request.POST or None)
     if form.is_valid():
@@ -29,6 +34,8 @@ def add(request):
     return render(request, 'coffees/add.html', {'form': form})
 
 
+@login_required
+@permission_required('coffees.edit_coffee', raise_exception=True)
 def edit(request, pk):
     coffee = get_object_or_404(Coffee, pk=pk)
     form = CoffeeForm(request.POST or None, instance=coffee)
@@ -39,6 +46,8 @@ def edit(request, pk):
     return render(request, 'coffees/edit.html', {'form': form})
 
 
+@login_required
+@permission_required('coffees.delete_coffee', raise_exception=True)
 def delete(request, pk):
     coffee = get_object_or_404(Coffee, pk=pk)
     form = DeleteConfirmForm(request.POST or None)
